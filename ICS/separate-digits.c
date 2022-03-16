ac0425c655966d7ad0793e7534a7be6ba8f1260f
skbtemp12 <73464906+skbtemp12@users.noreply.github.com>
2022-03-16 23:34:36 +0600

    Update separate-digits.c

--- ICS/separate-digits.c
+++ ICS/separate-digits.c
@@ -3,23 +3,20 @@
 
 int main()
 {
-    printf("enter 3 digit integer Number: ");
     int a,b,c,num,result;
-
+    printf("enter 3 digit integer Number: ");
     scanf("%d", &num);
 
+    // algorithm
     a=num%10;
     result=num/10;
-
+    
     b=result%10;
     result=result/10;
 
     c=result%10;
     result=result/10; // now result == 0
 
-
     printf("digits are: %d, %d, %d", a,b,c);
-
-
     return 0;
 }
